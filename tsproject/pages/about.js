45258eb20970061f4a98bf62f630820d15ec089a
이현종 <62270266+Brem0827@users.noreply.github.com>
2023-09-07 16:40:32 +0900

    Update Nextjs

--- tsproject/pages/about.js
+++ tsproject/pages/about.js
@@ -18,7 +18,7 @@ export default function Home() {
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
+            <code className={styles.code}>pages/about.js</code>
           </p>
           <p>
             <a href='./index.js'>Home</a> | <a href='./about.js'>about</a> | <a href='./page1.js'>page1</a>
