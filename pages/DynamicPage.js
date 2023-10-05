cf52ddc589cfd0a9d305f4b402fc143dfd92c75a
이현종 <62270266+Brem0827@users.noreply.github.com>
2023-10-05 14:14:10 +0900

    Update

---
+++ pages/DynamicPage.js
@@ -0,0 +1,18 @@
+import dynamic from "next/dynamic";
+
+const Highlight = dynamic(
+    () => import('../components/Highlight'),
+    { ssr: false }
+);
+
+import styles from '../styles/Home.module.css';
+
+function DynamicPage() {
+    return (
+        <div className={styles.main}>
+            <Highlight code={`console.log("Hello, World!")`} language="js" />
+        </div>
+    );
+}
+
+export default DynamicPage;
