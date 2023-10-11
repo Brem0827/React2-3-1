c81380e4c722a7740caa489dd8bec6af26a74c66
Brem <62270266+Brem0827@users.noreply.github.com>
2023-10-11 18:25:09 +0900

    Update Chapter02_06.js

--- pages/Chapter02_06.js
+++ pages/Chapter02_06.js
@@ -1,21 +1,23 @@
-export async function getStaticPtops() {
-  const res = await fetch('https://api.github.com/repos/vercel/next.js')
-  const repo = await res.json()
-
-  return {
-    props : {repo},
-      revalidate : 60,
-  }
+export async function getStaticProps() {
+    const res = await fetch('https://api.github.com/repos/vercel/next.js');
+    const repo = await res.json();
+    return {
+        props: { repo },
+        revalidate: 60,
+    }
 }
 
+import styles from '../styles/Home.module.css'
+
 export default function Chapter02_06({repo}) {
-  return (
-    <>
-      <div>
-        <h1>{repo.name}</h1>
-        <h1>{repo.node_id}</h1>
-        <h1>{repo.node_url}</h1>
-      </div>
-    </>
-  )
+    return (
+        <>
+            {/* Next 공식 문서 예제 */}
+            <div className={styles.main}>
+                <h1>{repo.name}</h1>
+                <h1>{repo.node_id}</h1>
+                <h1>{repo.html_url}</h1>
+            </div>
+        </>
+    )
 }
